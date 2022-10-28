b076442207b5d9c36bc6c41a83f1e8d50a73d7f4
Rayhan <rayhanprima99@gmail.com>
2022-10-28 20:38:42 +0700

    [CHORE] Add base redux configuration

---
+++ src/store/user/user.actions.js
@@ -0,0 +1,34 @@
+import { GET_ALL_USERS, GET_DETAILS_USER } from './user.types';
+import axios from 'axios';
+
+// This function will be called in component and it will triggered the reducers
+export const getAllUsers = () => async (dispatch) => {
+	try {
+		// Imagize we get data from API (the variable is users)
+		const { data } = await axios.get('https://jsonplaceholder.typicode.com/users');
+
+		// Dispatch to reducers
+		dispatch({
+			type: GET_ALL_USERS,
+			payload: data,
+		});
+	} catch (error) {
+		throw error;
+	}
+};
+
+export const getDetailsUser = (id) => async (dispatch) => {
+	try {
+		// Imagize we get data from API (the variable is users)
+		const { data } = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+		// https://jsonplaceholder.typicode.com/users/1
+
+		// Dispatch to reducers
+		dispatch({
+			type: GET_DETAILS_USER,
+			payload: data,
+		});
+	} catch (error) {
+		throw error;
+	}
+};
