ff289ad11ae16b71cb7cfce91e37101662b377cc
arayhan <rayhanprima99@gmail.com>
2022-12-07 19:22:35 +0700

    refactor: change Home component to default export

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export const Home = () => {
+const Home = () => {
 	return (
 		<div>
 			<div className="p-20 text-white bg-primary">Project Setup Finished</div>
 		</div>
 	);
 };
+
+export default Home;
