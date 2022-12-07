4f59d74efa46f4be83aa54b71b95f7a0157f5902
arayhan <rayhanprima99@gmail.com>
2022-12-08 02:13:38 +0700

    feat: home
    - add home banner component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,10 @@
+import { BannerHome } from '@/components/molecules';
 import React from 'react';
 
 const Home = () => {
 	return (
 		<div>
-			<div className="p-20 text-white bg-primary">
-				<h1 className="text-4xl">Home</h1>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam sit labore dignissimos debitis sint modi
-					rerum facilis saepe dolorum suscipit at repellendus ut, fugiat harum voluptates veniam eligendi neque tempore!
-				</p>
-			</div>
+			<BannerHome />
 		</div>
 	);
 };
