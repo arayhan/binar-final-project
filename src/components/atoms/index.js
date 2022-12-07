dadde277e9c2ecad4207d4e7e8467bd45f8d54f9
arayhan <rayhanprima99@gmail.com>
2022-12-08 05:29:23 +0700

    chore: add atoms component - Alert

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,5 +1,5 @@
+export * from './Alert/Alert';
 export * from './Button/Button';
-
 export * from './InputError/InputError';
 export * from './InputLabel/InputLabel';
 export * from './InputText/InputText';
