60f36e0df0347d39b4ae7f15b64fe7d52b1423a8
arayhan <rayhanprima99@gmail.com>
2022-12-08 00:46:44 +0700

    chore: add routing behavior

---
+++ src/components/layouts/MainLayout/MainLayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { MainHeader } from './MainHeader';
+
+export const MainLayout = () => {
+	return (
+		<div>
+			<MainHeader />
+
+			<Outlet />
+		</div>
+	);
+};
