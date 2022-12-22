9f872c50a6e9b575be947908839ce331af13449c
arayhan <rayhanprima99@gmail.com>
2022-12-23 00:05:40 +0700

    [REFACTOR] InputSelect : rename boolean props

--- src/components/atoms/InputSelect/InputSelect.jsx
+++ src/components/atoms/InputSelect/InputSelect.jsx
@@ -2,37 +2,42 @@ import React, { forwardRef } from 'react';
 import ReactSelect from 'react-select';
 import './InputSelect.css';
 
-export const InputSelect = forwardRef(({ options, value, loading, multi, searchable, disabled, onChange, placeholder, ...props }, ref) => {
-	return (
-		<ReactSelect
-			{...props}
-			ref={ref}
-			value={options.filter(function (option) {
-				return option.value === value;
-			})}
-			className={`z-10 text-[1.1rem] ${props.className}`}
-			placeholder={placeholder}
-			onChange={onChange}
-			isMulti={multi}
-			isSearchable={searchable}
-			isLoading={loading}
-			isDisabled={disabled}
-			isClearable
-			styles={{
-				input: (provided) => ({
-					...provided,
-					'input:focus': {
-						boxShadow: 'none',
-						zIndex: 20
-					}
-				}),
-				menu: (styles) => ({ ...styles, zIndex: 30, fontSize: '1.1rem' }),
-				menuPortal: (styles) => ({ ...styles, zIndex: 30 })
-			}}
-			menuPortalTarget={document.body}
-			options={options}
-		/>
-	);
-});
+export const InputSelect = forwardRef(
+	({ options, value, isLoading, isMulti, isSearchable, isClearable, disabled, onChange, placeholder, ...props }, ref) => {
+		return (
+			<ReactSelect
+				{...props}
+				ref={ref}
+				value={options.filter(function (option) {
+					return option.value === value;
+				})}
+				className={`z-10 text-[1.1rem] ${props.className}`}
+				placeholder={placeholder}
+				onChange={onChange}
+				isMulti={isMulti}
+				isSearchable={isSearchable}
+				isLoading={isLoading}
+				isDisabled={disabled}
+				isClearable={isClearable}
+				styles={{
+					input: (provided) => ({
+						...provided,
+						'input:focus': {
+							boxShadow: 'none',
+							zIndex: 20
+						}
+					}),
+					menu: (styles) => ({ ...styles, zIndex: 30, fontSize: '1.1rem' }),
+					menuPortal: (styles) => ({ ...styles, zIndex: 30 })
+				}}
+				menuPortalTarget={document.body}
+				options={options}
+			/>
+		);
+	}
+);
 
 InputSelect.displayName = 'InputSelect';
+InputSelect.defaultProps = {
+	isSearchable: true
+};
