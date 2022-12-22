74b7917b9e901473bdec6058949786fe0f5b217a
arayhan <rayhanprima99@gmail.com>
2022-12-23 02:21:09 +0700

    [FEAT] Home - Search Flights
    - divide form into single component
    - rename CardFormBooking to CardFormSearchFlights

--- src/components/molecules/Banner/BannerHome/BannerHome.jsx
+++ src/components/molecules/Banner/BannerHome/BannerHome.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { CardFormBooking } from '../../Card/CardFormBooking/CardFormBooking';
+import { CardFormSearchFlights } from '../../Card/CardFormSearchFlights/CardFormSearchFlights';
 
 export const BannerHome = () => {
 	return (
 		<div className="bg-[url('@/images/pictures/pulau_padar.jpg')] bg-slate-100 bg-fixed bg-no-repeat bg-cover bg-bottom bg-blend-multiply filter grayscale-[0.3]">
 			<div className="container py-32">
 				<div className="flex flex-col lg:flex-row lg:items-center justify-between gap-8">
-					<CardFormBooking />
+					<CardFormSearchFlights />
 				</div>
 			</div>
 		</div>
--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,5 +1,5 @@
 export * from './Banner/BannerHome/BannerHome';
 
-export * from './Card/CardFormBooking/CardFormBooking';
+export * from './Card/CardFormSearchFlights/CardFormSearchFlights';
 
 export * from './Section/SectionFeatures/SectionFeatures';
