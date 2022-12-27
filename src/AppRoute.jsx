e136d8a838e2390e1c45b183a30c568e07d76110
Siti Solihat Firdaus <sitisolihatfirdaus@gmail.com>
2022-12-28 01:30:11 +0700

    [CHORE] Profile: Update Account Information

--- src/AppRoute.jsx
+++ src/AppRoute.jsx
@@ -2,16 +2,16 @@ import { useSelector } from 'react-redux';
 import { Navigate, Outlet, Route, Routes as RoutesContainer } from 'react-router-dom';
 import { AuthLayout } from './components/layouts/AuthLayout/AuthLayout';
 import { MainLayout } from './components/layouts/MainLayout/MainLayout';
-import { ProfileLayout } from './components/layouts/ProfileLayout/ProfileLayout';
+import { ProfileLayout } from './components/layouts/ProfileLayout/ProfileLayout/ProfileLayout';
 import { ROUTES } from './configs/routes';
 import Dashboard from './pages/Admin/Dashboard/Dashboard';
 import Login from './pages/Auth/Login/Login';
 import Register from './pages/Auth/Register/Register';
 import Home from './pages/Home/Home';
-import ProfileSidebar from './pages/Profile/ProfileSidebar';
 import ProfileMyAccount from './pages/Profile/ProfileMyAccount/ProfileMyAccount';
-import ProfilePurchaseList from './pages/Profile/ProfileMyAccount/ProfileMyAccount';
-import ProfileMyPoints from './pages/Profile/ProfileMyPoints/ProfileMyPoints';
+import ProfilePurchaseList from './pages/Profile/ProfilePurchaseList/ProfilePurchaseList';
+import ProfileMyOrder from './pages/Profile/ProfileMyOrder/ProfileMyOrder';
+import ProfileMyBilling from './pages/Profile/ProfileMyBilling/ProfileMyBilling';
 
 const AppRoute = () => {
 	const { isAuthenticated } = useSelector((state) => state.auth);
@@ -26,27 +26,6 @@ const AppRoute = () => {
 
 	return (
 		<RoutesContainer>
-<<<<<<< HEAD
-			<Route element={<ProtectedRoute />}>
-				<Route path={PATH.ADMIN} element={<Dashboard />} />
-			</Route>
-
-			<Route element={<AuthenticationRoute />}>
-				<Route element={<AuthLayout />}>
-					<Route path={PATH.LOGIN} element={<Login />} />
-					<Route path={PATH.REGISTER} element={<Register />} />
-				</Route>
-			</Route>
-
-			<Route element={<ProtectedRoute />}>
-				<Route path={ROUTES.ADMIN.path} element={<Dashboard />} />
-			</Route>
-
-			<Route element={<ProfileLayout />}>
-				<Route path={ROUTES.PROFILE.path} element={<ProfileSidebar />} />
-			</Route>
-
-=======
 			<Route element={<AuthenticationRoute />}>
 				<Route element={<AuthLayout />}>
 					<Route path={ROUTES.LOGIN.path} element={<Login />} />
@@ -54,15 +33,20 @@ const AppRoute = () => {
 				</Route>
 			</Route>
 
->>>>>>> f815a059df4c28e5fbbeb520b2e0ef557757d6b5
+			<Route element={<ProtectedRoute />}>
+				<Route path={ROUTES.ADMIN.path} element={<Dashboard />} />
+			</Route>
+
 			<Route element={<MainLayout />}>
-				<Route path={PATH.HOME} element={<Home />} />
-				<Route path={PATH.PROFILE} element={<ProfileLayout />}>
-					<Route path={PATH.PROFILE} element={<ProfileMyAccount />} />
-					<Route path={PATH.PROFILE_MY_ACCOUNT} element={<ProfileMyAccount />} />
-					<Route path={PATH.PROFILE_PURCHASE_LIST} element={<ProfilePurchaseList />} />
-					<Route path={PATH.PROFILE_MY_POINTS} element={<ProfileMyPoints />} />
-				</Route>
+				<Route path={ROUTES.HOME.path} element={<Home />} />
+			</Route>
+
+			<Route element={<ProfileLayout />}>
+				<Route path={ROUTES.PROFILE.path} element={<ProfileMyAccount />} />
+				<Route path={ROUTES.PROFILE_MY_ACCOUNT.path} element={<ProfileMyAccount />} />
+				<Route path={ROUTES.PROFILE_PURCHASE_LIST.path} element={<ProfilePurchaseList />} />
+				<Route path={ROUTES.PROFILE_MY_ORDER.path} element={<ProfileMyOrder />} />
+				<Route path={ROUTES.PROFILE_MY_BILLING.path} element={<ProfileMyBilling />} />
 			</Route>
 		</RoutesContainer>
 	);
---
+++ src/components/molecules/InputSelect/InputSelectGender/InputSelectGender.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Select from 'react-select';
+
+const genderOptions = [
+	{ value: 'female', label: 'Female' },
+	{ value: 'male', label: 'Male' }
+];
+
+const GenderSelect = () => {
+	return (
+		<div>
+			<h1 className="text-gray-500 text-sm pb-2">Gender</h1>
+			<div className="lg:w-[40rem]">
+				<Select options={genderOptions} placeholder="Select Gender" />
+			</div>
+		</div>
+	);
+};
+
+export default GenderSelect;
