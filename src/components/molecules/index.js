0ff8b6378d1feb851ed773add8954c5b601eca5f
Ahmed Rayhan Primadedas <rayhanprima99@gmail.com>
2022-12-28 22:06:55 +0700

    [CHORE] add InputSelectPersonTitle component

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -2,5 +2,7 @@ export * from './Banner/BannerHome/BannerHome';
 
 export * from './Card/CardFormSearchFlights/CardFormSearchFlights';
 
+export * from './InputSelect/InputSelectPersonTitle/InputSelectPersonTitle';
+
 export * from './Section/SectionFeatures/SectionFeatures';
 export * from './Section/SectionInspiration/SectionInspiration';
