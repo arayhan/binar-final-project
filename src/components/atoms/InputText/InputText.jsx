487a893ced16018063cebbf2103a955561ff19b3
Ahmed Rayhan Primadedas <rayhanprima99@gmail.com>
2022-12-28 22:07:34 +0700

    [CHORE] InputText & InputLabel
    - add props helper on label
    - add props prefix on inputtext

--- src/components/atoms/InputText/InputText.jsx
+++ src/components/atoms/InputText/InputText.jsx
@@ -2,19 +2,22 @@ import { forwardRef } from 'react';
 import { InputError } from '../InputError/InputError';
 import { InputLabel } from '../InputLabel/InputLabel';
 
-export const InputText = forwardRef(({ label, placeholder, error, ...props }, ref) => {
+export const InputText = forwardRef(({ label, helper, prefix, placeholder, error, ...props }, ref) => {
 	return (
 		<div className="space-y-2 flex flex-col justify-start">
-			<InputLabel text={label} name={props.name} />
-			<input
-				{...props}
-				ref={ref}
-				className={`border ${
-					error ? 'border-red-500' : 'border-gray-300'
-				} rounded-[4px] px-3 py-[6px] focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 placeholder:text-gray-400 disabled:bg-gray-100 disabled:border-gray-200 disabled:text-gray-400`}
-				placeholder={placeholder}
-				id={props.name}
-			/>
+			<InputLabel text={label} name={props.name} helper={helper} />
+			<div className="flex items-center space-x-4">
+				{prefix && <div>{prefix}</div>}
+				<input
+					{...props}
+					ref={ref}
+					className={`w-full border ${
+						error ? 'border-red-500' : 'border-gray-300'
+					} rounded-[4px] px-3 py-[7px] focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 placeholder:text-gray-400 disabled:bg-gray-100 disabled:border-gray-200 disabled:text-gray-400`}
+					placeholder={placeholder}
+					id={props.name}
+				/>
+			</div>
 			{error && <InputError message={error.message} />}
 		</div>
 	);
