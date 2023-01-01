25fc99f18db8c4d38cbe8af2240e62c8aa41e6d6
Ahmed Rayhan Primadedas <rayhanprima99@gmail.com>
2023-01-01 17:51:29 +0700

    [FEAT] Auth
    - add redirect to login with google

--- src/components/atoms/Button/ButtonLoginWithGoogle.jsx
+++ src/components/atoms/Button/ButtonLoginWithGoogle.jsx
@@ -1,5 +1,7 @@
+import { PATH } from '@/configs/routes';
 import { ACTION_AUTH } from '@/store/actions';
 import { LOGIN_METHODS } from '@/utils/constants';
+import { queryStringToObject } from '@/utils/helpers';
 import { GoogleLogin } from '@react-oauth/google';
 import React from 'react';
 import { notify } from 'react-notify-toast';
@@ -13,6 +15,11 @@ export const ButtonLoginWithGoogle = () => {
 		dispatch(
 			actionLogin({ credential: response.credential }, LOGIN_METHODS.GOOGLE, ({ success, message }) => {
 				if (message) notify.show(message, success ? 'success' : 'error');
+				if (success) {
+					const queryParams = location.search ? queryStringToObject(location.search) : null;
+					const redirect = queryParams?.redirect || PATH.HOME;
+					window.location.href = redirect;
+				}
 			})
 		);
 	};
