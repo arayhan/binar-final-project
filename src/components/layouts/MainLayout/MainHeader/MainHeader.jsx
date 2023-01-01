e494b29db4a253fb59661c986fef90025a69cf42
Ahmed Rayhan Primadedas <rayhanprima99@gmail.com>
2023-01-02 01:54:11 +0700

    [CHORE] MainHeader : update dropdown button on profile header

--- src/components/layouts/MainLayout/MainHeader/MainHeader.jsx
+++ src/components/layouts/MainLayout/MainHeader/MainHeader.jsx
@@ -1,9 +1,13 @@
 import { PATH } from '@/configs/routes';
 import { Link, useNavigate } from 'react-router-dom';
 import { FaChevronDown, FaUserCircle } from 'react-icons/fa';
+import { GoSignOut } from 'react-icons/go';
 import { MainHeaderLogo } from './MainHeaderLogo';
 import { useDispatch, useSelector } from 'react-redux';
 import { ACTION_AUTH } from '@/store/actions';
+import { RiHomeLine } from 'react-icons/ri';
+import { useState } from 'react';
+import { useDetectClickOutside } from 'react-detect-click-outside';
 
 export const MainHeader = () => {
 	const dispatch = useDispatch();
@@ -12,6 +16,12 @@ export const MainHeader = () => {
 	const { user, isAuthenticated } = useSelector((state) => state.auth);
 
 	const { actionLogout } = ACTION_AUTH;
+
+	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+	const dropdownItemClasses = 'flex items-center space-x-3 p-3 text-left hover:bg-gray-100 text-gray-600';
+
+	const dropdownRef = useDetectClickOutside({ onTriggered: () => setIsDropdownOpen(false) });
 
 	const handleLogout = () =>
 		dispatch(
@@ -28,7 +38,7 @@ export const MainHeader = () => {
 					<MainHeaderLogo />
 				</div>
 				<div>
-					<div className="flex items-center space-x-4">
+					<div className="relative z-30 flex items-center space-x-4">
 						{!isAuthenticated && (
 							<Link
 								className="flex items-center gap-3 border border-primary text-primary px-4 py-2 rounded-full hover:bg-primary hover:text-white"
@@ -39,11 +49,29 @@ export const MainHeader = () => {
 							</Link>
 						)}
 						{isAuthenticated && (
-							<button className="flex items-center gap-2 px-6 py-3 rounded-md hover:bg-gray-100 transition-all" onClick={handleLogout}>
+							<button
+								ref={dropdownRef}
+								className="flex items-center gap-2 px-6 py-3 rounded-md hover:bg-gray-100 transition-all"
+								onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+							>
 								<FaUserCircle className="text-primary" size={22} />
 								<span className="">{user ? user.name || user.username : `{{username}}`}</span>
 								<FaChevronDown className="text-gray-500" size={10} />
 							</button>
+						)}
+						{isDropdownOpen && (
+							<div className="absolute right-0 bottom-0 transform translate-y-full w-48 bg-white rounded-sm shadow-lg overflow-hidden">
+								<div className="flex flex-col divide-y text-xs md:text-sm">
+									<button className={dropdownItemClasses} onClick={() => navigate('/')}>
+										<RiHomeLine />
+										<span>Home</span>
+									</button>
+									<button className={dropdownItemClasses} onClick={handleLogout}>
+										<GoSignOut />
+										<span>Logout</span>
+									</button>
+								</div>
+							</div>
 						)}
 					</div>
 				</div>
