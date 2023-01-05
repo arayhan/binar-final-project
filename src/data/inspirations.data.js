07e886a6e1784ec1aeee67bc56d52c93dc68f35f
Ahmed Rayhan Primadedas <rayhanprima99@gmail.com>
2023-01-05 20:01:18 +0700

    [CHORE] Update Inspiration data on home

--- src/data/inspirations.data.js
+++ src/data/inspirations.data.js
@@ -7,44 +7,14 @@ export const DATA_INSPIRATIONS = [
 	},
 	{
 		uuid: 'fad5ca3d-9b13-4520-8e1f-da01a86c0d47',
-		thumbnail: 'https://blog.antavaya.com/wp-content/uploads/2021/07/15-Tempat-Wisata-Bangka-Belitung-Terbaru-Paling-Hits-2021.jpg',
-		title: 'Bangka Belitung',
-		price: 'Rp 2.000.000'
+		thumbnail: 'https://www.javatravel.net/wp-content/uploads/2019/07/Tempat-Wisata-Surabaya.jpg',
+		title: 'Surabaya',
+		price: 'Rp 2.500.000'
 	},
 	{
 		uuid: '04021f35-1dcf-4d57-b39d-5f87d4f57f2f',
-		thumbnail: 'https://blog.antavaya.com/wp-content/uploads/2021/07/15-Tempat-Wisata-Bangka-Belitung-Terbaru-Paling-Hits-2021.jpg',
-		title: 'Bangka Belitung',
-		price: 'Rp 2.000.000'
-	},
-	{
-		uuid: '8503b044-d6c1-476c-9859-18c116ee73b8',
-		thumbnail: 'https://blog.antavaya.com/wp-content/uploads/2021/07/15-Tempat-Wisata-Bangka-Belitung-Terbaru-Paling-Hits-2021.jpg',
-		title: 'Bangka Belitung',
-		price: 'Rp 2.000.000'
-	},
-	{
-		uuid: '6b37340d-050b-42a9-a907-a34b039b88cf',
-		thumbnail: 'https://blog.antavaya.com/wp-content/uploads/2021/07/15-Tempat-Wisata-Bangka-Belitung-Terbaru-Paling-Hits-2021.jpg',
-		title: 'Bangka Belitung',
-		price: 'Rp 2.000.000'
-	},
-	{
-		uuid: '8111c087-1534-4be6-8772-3bc6681ea3d8',
-		thumbnail: 'https://blog.antavaya.com/wp-content/uploads/2021/07/15-Tempat-Wisata-Bangka-Belitung-Terbaru-Paling-Hits-2021.jpg',
-		title: 'Bangka Belitung',
-		price: 'Rp 2.000.000'
-	},
-	{
-		uuid: 'fd1582e1-01d8-4c1d-baac-613d1b7eba13',
-		thumbnail: 'https://blog.antavaya.com/wp-content/uploads/2021/07/15-Tempat-Wisata-Bangka-Belitung-Terbaru-Paling-Hits-2021.jpg',
-		title: 'Bangka Belitung',
-		price: 'Rp 2.000.000'
-	},
-	{
-		uuid: '3e00be17-1eaa-4229-9e82-b04917c3289d',
-		thumbnail: 'https://blog.antavaya.com/wp-content/uploads/2021/07/15-Tempat-Wisata-Bangka-Belitung-Terbaru-Paling-Hits-2021.jpg',
-		title: 'Bangka Belitung',
-		price: 'Rp 2.000.000'
+		thumbnail: 'https://img.inews.co.id/media/600/files/inews_new/2021/09/20/10_tempat_Wisata_di_Makassar.JPG',
+		title: 'Makassar',
+		price: 'Rp 1.800.000'
 	}
 ];
